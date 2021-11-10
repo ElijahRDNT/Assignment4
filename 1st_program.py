8144eaf20f27698b3d804fd2afa6d20ad1706b01
LAPTOP-7O8QBV3P\lenovo <elijahmaetardiente@iskolarngbayan.pup.edu.ph>
2021-11-10 19:59:13 +0800

    first commit for 1st_program.py

---
+++ 1st_program.py
@@ -0,0 +1,7 @@
+#Assignment: create a function that returns multiple values (move all user inputs in one function)
+name = input("Please enter you name: ")
+age = input("Please enter your age in years: ")
+address = input("Please enter your address: ")
+
+print("\nHi, my name is " + name + ". I am " + age +  # this is the final output
+          " years old and I live in " + address + ".")
