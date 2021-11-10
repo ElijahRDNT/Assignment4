b7e5664d2a62bed268e9ae75a6694bf71af67460
LAPTOP-7O8QBV3P\lenovo <elijahmaetardiente@iskolarngbayan.pup.edu.ph>
2021-11-10 20:28:58 +0800

    added a function for different age validations

--- 1st_program.py
+++ 1st_program.py
@@ -9,5 +9,19 @@ def display(name, age, address):
     print("\nHi, my name is " + name + ". I am " + age +  # this is the final output
             " years old and I live in " + address + ".")
 
+def age_validation(nameF, ageF, addressF):  # this function contains validation for age input such as the data type or value
+    try:  # the system will try the set of codes under "try". If can't be executed, it will proceed to "except"
+        age_int = int(ageF)
+
+        if(age_int <= 0):  # age input that is less than or equal to zero is invalid
+            print("\nInvalid age.")
+        else:
+            # prompts the system to execute the display() function
+            display(nameF, ageF, addressF)
+
+    # this will be executed if the age can't be converted into an integer type (ex. if the input is in letters)
+    except ValueError:
+        print("\nError. Only whole numbers are accepted for age.")
+
 name_g, age_g, address_g = get_user_inputs()
-display(name_g, age_g, address_g)
+age_validation(name_g, age_g, address_g)
