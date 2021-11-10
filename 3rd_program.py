28f1b0263550ec008ea05923daa93ddacdee5fd1
LAPTOP-7O8QBV3P\lenovo <elijahmaetardiente@iskolarngbayan.pup.edu.ph>
2021-11-10 22:23:49 +0800

    added input validations and restrictions

--- 3rd_program.py
+++ 3rd_program.py
@@ -1,18 +1,34 @@
 # Assignment: create a function that returns multiple values (move all user inputs in one function)
-def get_user_inputs(): 
-    money_input = float(  # float is used to enable decimal points because money is the subject
-        input("Please enter the amount of money you have: "))
-    apple_input = float(  # float is used to enable decimal points because money is the subject if talking about price
-        input("Please enter the price of an apple that you would like to buy: "))
-    return money_input, apple_input    
+try:
+    
+    def get_user_inputs(): 
+        money_input = float(  # float is used to enable decimal points because money is the subject
+            input("Please enter the amount of money you have: "))
+        apple_input = float(  # float is used to enable decimal points because money is the subject if talking about price
+            input("Please enter the price of an apple that you would like to buy: "))
 
-def final_output(moneyF, appleF):  # function for computation
-    # this is converted to integer data type to avoid decimal places while flashing no. of apples
-    max_apple = int(moneyF//appleF)
-    change = moneyF % appleF
+        if(apple_input == 0.0):  # restricts user to enter 0.00 priced apples
+            print("Sorry, we don't have apples that cost 0.00 pesos.")
+            quit()  # prompt to terminate the program
+        else:
+            return money_input, apple_input    
 
-    print(
-    "You can buy " + str(max_apple) + " apples and your change is " + str(change) + " pesos.")
+    def final_output(moneyF, appleF):  # function for computation
+        # this is converted to integer data type to avoid decimal places while flashing no. of apples
+        max_apple = int(moneyF//appleF)
+        change = moneyF % appleF
 
-money, apple = get_user_inputs()
-final_output(money, apple)
+        # target final output
+        print(
+            f"You can buy {max_apple} apples and your change is {change:.2f} pesos.")
+
+    money, apple = get_user_inputs()
+
+    if(money < 0 or apple < 0):  # restricts the user to enter less than 0 inputs
+        print("Error. Less than 0 inputs are invalid.")
+    else:
+        # prompts the system to execute the final_output function
+        final_output(money, apple)
+
+except ValueError:  # this will be executed if the input datatype is incorrect or the input is empty
+    print("Invalid input. Please input a number.")
